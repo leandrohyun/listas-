6d3f64d31aea3f4bea48065539c6b76b5ad1d895
leandrohyun <101629903+leandrohyun@users.noreply.github.com>
2022-05-16 22:37:27 -0300

    Update listasligadasordenadas.js

--- listasligadasordenadas.js
+++ listasligadasordenadas.js
@@ -1,7 +1,85 @@
 /*
-No jogo existe uma ferramenta chamada “zoneamento”, onde é possível destinar certo 
-local para determinadas construções, sendo ela residencial, comercial e industrial
-podendo ser de alta ou baixa demanda.
-As listas circulares serão usadas para armazenar os prédios de um quarteirão que seja
-do mesmo zoneamento.
-*/
+No jogo as listas ligadas ordenadas seguirão o mesmo padrão anterior,
+ porém nesse caso, a lista será ordenada de acordo com a quantidade 
+ de moradores em cada prédio. 
+ */
+ class LinkedList {
+    constructor(equalsFn = defaultEquals) {
+      this.equalsFn = equalsFn;
+      this.count = 0;
+      this.head = undefined;
+    }
+    }
+const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1,
+    EQUALS: 0
+  };
+  
+ function defaultCompare(a, b) {
+    if (a === b) {
+      return Compare.EQUALS;
+    }
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+  }
+  
+  function defaultEquals(a, b) {
+    return a === b;
+  }
+
+ class SortedLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals, compareFn = defaultCompare) {
+    super(equalsFn);
+    this.equalsFn = equalsFn;
+    this.compareFn = compareFn;
+  }
+  push(element) {
+    if (this.isEmpty()) {
+      super.push(element);
+    } else {
+      const index = this.getIndexNextSortedElement(element);
+      super.insert(element, index);
+    }
+  }
+  insert(element, index = 0) {
+    if (this.isEmpty()) {
+      return super.insert(element, index === 0 ? index : 0);
+    }
+    const pos = this.getIndexNextSortedElement(element);
+    return super.insert(element, pos);
+  }
+  getIndexNextSortedElement(element) {
+    let current = this.head;
+    let i = 0;
+    for (; i < this.size() && current; i++) {
+      const comp = this.compareFn(element, current.element);
+      if (comp === Compare.LESS_THAN) {
+        return i;
+      }
+      current = current.next;
+    }
+    return i;
+  }
+  isEmpty() {
+    return this.count === 0;
+  }
+  size() {
+    return this.count;
+  }
+}
+
+
+const list = new SortedLinkedList();
+
+//push
+list.push(12);
+list.push(30);
+list.push(23);
+list.push(10);
+list.push(27);
+console.log(list);
+
+//Insert
+
+list.insert (200, 1);
+console.log(list);

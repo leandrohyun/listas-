f29fe07ea5dd0f15f111c6aade239c25dd7cbdcb
leandrohyun <101629903+leandrohyun@users.noreply.github.com>
2022-05-16 22:34:56 -0300

    Update listasduplamenteligadas.js

--- listasduplamenteligadas.js
+++ listasduplamenteligadas.js
@@ -1,3 +1,9 @@
+/*
+No cities skylines as listas ligadas serão utilizadas para gerenciar
+ os prédios em uma avenida entre dois cruzamentos já que por ter duas 
+ entradas e saídas nesse local da avenida a contagem da lista pode começar 
+ por qualquer lado.
+ */
 function defaultEquals(a, b) {
     return a === b;
   }
